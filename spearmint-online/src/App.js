fb63ad838bfa6f98e1c15e5abe48271bad67a978
santosh214 <santoshsingh74827@gmail.com>
2023-08-25 01:10:59 +0530

    setup game

---
+++ spearmint-online/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+
+function App() {
+  return (
+   <section id='mainSection'>
+   <Navbar/>
+   </section>
+  );
+}
+
+export default App;
---
+++ spearmint-online/src/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { init } from '@web3-onboard/react';
+import injectedModule from '@web3-onboard/injected-wallets';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
+import { BrowserRouter } from 'react-router-dom';
+
+
+const injected = injectedModule();
+
+init({
+  wallets: [injected],
+  chains: [
+    {
+      id: '0x38',
+      token: 'BNB',
+      label: 'BNB Chain',
+      rpcUrl: 'https://bsc-dataseed.binance.org/',
+    },
+    {
+      id: '0x89',
+      token: 'MATIC',
+      label: 'Matic Mainnet',
+      rpcUrl: 'https://matic-mainnet.chainstacklabs.com',
+    },
+    {
+      id: '0xfa',
+      token: 'FTM',
+      label: 'Fantom Mainnet',
+      rpcUrl: 'https://rpc.ftm.tools/',
+    },
+  ],
+  accountCenter: {
+    mobile: { enabled: false },
+    desktop: { enabled: false },
+  },
+  theme: 'dark',
+});
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+    <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
